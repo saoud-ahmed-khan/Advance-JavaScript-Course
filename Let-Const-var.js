0a875f9866bd6b13aa87b3028dfb1f0dbd222a9d
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2020-12-17 19:12:17 +0500

    Initial commit

---
+++ Let-Const-var.js
@@ -0,0 +1,23 @@
+// //var
+// var a=2;
+// // console.log(a)
+// function print()
+// {
+//     console.log("var inside a function " + a);
+// }
+// print();
+
+//let
+
+
+// console.log(name);
+// function printname()
+// {
+//     let name="saoud"
+//     console.log("inside function " + name);
+// }
+// printname();
+ //const
+
+ const pi=3.142;
+ console.log(pi);
---
+++ scoping.js
@@ -0,0 +1,35 @@
+
+// var a="muneeb"
+
+// PrintName();
+
+// function PrintName()
+// {
+//     console.log(a);
+// }
+
+// console.log(names);
+// var names=function()
+// {
+//     console.log(a);
+// }
+// 
+//  scope chaining
+// function name()
+// {
+//     var a="kinza"
+//         function subname()
+//         { var a ="muneeb"
+//                 function subSubname()
+//                 {
+//                     console.log(a);
+//                 }
+//         }
+// }
+// var name="saoud";
+// console.log("my name is " + name);
+// console.log(`my name is ${name}`);
+
+var a;
+
+a?console.log(`my name is ${a}`):console.log("no value");
